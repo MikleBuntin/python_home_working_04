5a33f9c222ebed2c42fc39119a5d1ab9a1d1145d
MikleBuntin <BuntinMA@mail.ru>
2022-10-17 20:18:36 +0400

    Решил задачу 2.

--- Task_02/Task_02.py
+++ Task_02/Task_02.py
@@ -1,4 +1,22 @@
-# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# Задайте натуральное число N.
+# Напишите программу, которая составит список простых множителей числа N.
 # *Пример*
 # - при N=236     ->        [2, 2, 59]
+# 2,3,5,7,11,13
 
+number = int(input("Введите число: "))
+factor_list = [2, 3, 5, 7, 11, 13]
+factors = []
+
+for i in factor_list:
+    while(number % i == 0):
+        factors.append(i)
+        print("i = ", i)
+        print(number)
+        number = number // i
+        print(number)
+
+if number > 1:
+    factors.append(number)
+
+print(factors)
